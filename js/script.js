faa318506dbbb38610d6222db692cb908b774393
Arthur Soares <arthursoaressi@hotmail.com>
2025-06-01 18:28:23 -0300

    Creating the Game Over Screen Menu
    Criação do menu da tela de Game over

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext("2d")
+const score = document.querySelector(".score-value")
+const finalScore = document.querySelector(".final-score > span")
+const menu = document.querySelector(".menu-screen")
+const buttonPlay = document.querySelector(".btn-play")
 
 const audio = new Audio('../assets/eat.wav')
 
@@ -134,11 +138,13 @@ checkCollision = () => {
     if ((head.x < 0 || head.x > canvas.width - size || 
         head.y < 0 || head.y > canvas.width - size) ||
         (selfCollision)) {
-          alert("Game Over")  
+          gameOver()
         }
 }
 
-
+const gameOver = () => {
+    direction = undefined
+}
 
 
 
